f85ba1e92f6565ee890462cc151e19801ccdb3da
rene-ruiz <palaciosruiz.rene@gmail.com>
2024-08-21 21:42:39 -0600

    Modified action that creates favorites

--- console_list/api/viewsets.py
+++ console_list/api/viewsets.py
@@ -5,6 +5,8 @@ from console_list.models import ConsoleItem, UserConsole
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from rest_framework.status import HTTP_200_OK, HTTP_404_NOT_FOUND, HTTP_201_CREATED
+
 
 class ConsoleItemViewSet(ModelViewSet):
     queryset = ConsoleItem.objects.all()
@@ -25,8 +27,20 @@ class UserConsoleViewSet(ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-    @action(detail=False, methods=['get'], url_path='console/(?P<console_item_id>[^/.]+)')
-    def get_by_console_item(self, request, console_item_id=None):
-        user_console = get_object_or_404(UserConsole, console_item__id=console_item_id)
-        serializer = self.get_serializer(user_console)
-        return Response(serializer.data)
+    def create(self, request, *args, **kwargs):
+        user = request.user
+        console_item_id = request.data.get('console_item')
+
+        try:
+            console_item = ConsoleItem.objects.get(id=console_item_id)
+        except ConsoleItem.DoesNotExist:
+            return Response({"error": "ConsoleItem not found."}, status=HTTP_404_NOT_FOUND)
+
+        user_console = UserConsole.objects.filter(user=user, console_item=console_item).first()
+
+        if user_console:
+            user_console.delete()
+            return Response({"action": "removed"}, status=HTTP_200_OK)
+        else:
+            UserConsole.objects.create(user=user, console_item=console_item)
+            return Response({"action": "added"}, status=HTTP_201_CREATED)
